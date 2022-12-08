4ffdd247804c7bfd24381d3e9f7f77362c58799a
dhyani15 <nhd7682@nyu.edu>
2022-12-08 08:32:16 -0500

    implement train logic

---
+++ hateful_memes.py
@@ -0,0 +1,536 @@
+import json
+import logging
+from pathlib import Path
+import random
+import tarfile
+import tempfile
+import warnings
+import fasttext
+import torchvision
+import torch
+from PIL import Image
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pandas_path  # Path style access for pandas
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(
+                    prog = 'hateful_memes',
+                    description = 'Train multimodal architecture and generate adversarial attacks and then retrains'
+                    )
+parser.add_argument('--load','-l',action='store_true',help="loads a model and run attack scripts" )
+args = parser.parse_args()
+
+class HatefulMemesDataset(torch.utils.data.Dataset):
+    """Uses jsonl data to preprocess and serve 
+    dictionary of multimodal tensors for model input.
+    """
+
+    def __init__(
+        self,
+        data_path,
+        img_dir,
+        image_transform,
+        text_transform,
+        balance=False,
+        dev_limit=None,
+        random_state=0,
+    ):
+        
+        self.samples_frame = pd.read_json(
+            data_path, lines=True
+        )
+        self.dev_limit = dev_limit
+        if balance:
+            neg = self.samples_frame[
+                self.samples_frame.label.eq(0)
+            ]
+            pos = self.samples_frame[
+                self.samples_frame.label.eq(1)
+            ]
+            self.samples_frame = pd.concat(
+                [
+                    neg.sample(
+                        pos.shape[0], 
+                        random_state=random_state
+                    ), 
+                    pos
+                ]
+            )
+        if self.dev_limit:
+            if self.samples_frame.shape[0] > self.dev_limit:
+                self.samples_frame = self.samples_frame.sample(
+                    dev_limit, random_state=random_state
+                )
+        self.samples_frame = self.samples_frame.reset_index(
+            drop=True
+        )
+        self.samples_frame.img = self.samples_frame.apply(
+            lambda row: (img_dir / row.img), axis=1
+        )
+
+        # https://github.com/drivendataorg/pandas-path
+        # print(self.samples_frame.img)
+        # if not self.samples_frame.img.path.exists().all():
+        #     raise FileNotFoundError
+        # if not self.samples_frame.img.path.is_file().all():
+        #     raise TypeError
+            
+        self.image_transform = image_transform
+        self.text_transform = text_transform
+
+    def __len__(self):
+        """This method is called when you do len(instance) 
+        for an instance of this class.
+        """
+        return len(self.samples_frame)
+
+    def __getitem__(self, idx):
+        """This method is called when you do instance[key] 
+        for an instance of this class.
+        """
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_id = self.samples_frame.loc[idx, "id"]
+
+        image = Image.open(
+            self.samples_frame.loc[idx, "img"]
+        ).convert("RGB")
+        image = self.image_transform(image)
+        text = torch.Tensor(
+#             self.text_transform.wv[self.samples_frame.loc[idx, "text"]]
+            self.text_transform.get_sentence_vector(
+                self.samples_frame.loc[idx, "text"]
+            )
+        ).squeeze()
+
+        if "label" in self.samples_frame.columns:
+            label = torch.Tensor(
+                [self.samples_frame.loc[idx, "label"]]
+            ).long().squeeze()
+            sample = {
+                "id": img_id, 
+                "image": image, 
+                "text": text, 
+                "label": label
+            }
+        else:
+            sample = {
+                "id": img_id, 
+                "image": image, 
+                "text": text
+            }
+
+        return sample
+
+class LanguageAndVisionConcat(torch.nn.Module):
+    def __init__(
+        self,
+        num_classes,
+        loss_fn,
+        language_module,
+        vision_module,
+        language_feature_dim,
+        vision_feature_dim,
+        fusion_output_size,
+        dropout_p,
+        
+    ):
+        super(LanguageAndVisionConcat, self).__init__()
+        self.language_module = language_module
+        self.vision_module = vision_module
+        self.fusion = torch.nn.Linear(
+            in_features=(language_feature_dim + vision_feature_dim), 
+            out_features=fusion_output_size
+        )
+        self.fc = torch.nn.Linear(
+            in_features=fusion_output_size, 
+            out_features=num_classes
+        )
+        self.loss_fn = loss_fn
+        self.dropout = torch.nn.Dropout(dropout_p)
+        
+    def forward(self, text, image, label=None):
+        text_features = torch.nn.functional.relu(
+            self.language_module(text)
+        )
+        image_features = torch.nn.functional.relu(
+            self.vision_module(image)
+        )
+        combined = torch.cat(
+            [text_features, image_features], dim=1
+        )
+        fused = self.dropout(
+            torch.nn.functional.relu(
+            self.fusion(combined)
+            )
+        )
+        logits = self.fc(fused)
+        pred = torch.nn.functional.softmax(logits)
+        loss = (
+            self.loss_fn(pred, label) 
+            if label is not None else label
+        )
+        return (pred, loss)
+
+import pytorch_lightning as pl
+import warnings
+import logging
+
+# for the purposes of this post, we'll filter
+# much of the lovely logging info from our LightningModule
+warnings.filterwarnings("ignore")
+logging.getLogger().setLevel(logging.WARNING)
+
+
+class HatefulMemesModel(pl.LightningModule):
+    def __init__(self, hparams):
+        for data_key in ["train_path", "dev_path", "img_dir",]:
+            # ok, there's one for-loop but it doesn't count
+            if data_key not in hparams.keys():
+                raise KeyError(
+                    f"{data_key} is a required hparam in this model"
+                )
+        
+        super(HatefulMemesModel, self).__init__()
+        self.save_hyperparameters()
+        for key in hparams.keys():
+            self.hparams[key]=hparams[key]
+        
+        # assign some hparams that get used in multiple places
+        self.embedding_dim = self.hparams.get("embedding_dim", 300)
+        self.language_feature_dim = self.hparams.get(
+            "language_feature_dim", 300
+        )
+        self.vision_feature_dim = self.hparams.get(
+            # balance language and vision features by default
+            "vision_feature_dim", self.language_feature_dim
+        )
+        self.output_path = Path(
+            self.hparams.get("output_path", "model-outputs")
+        )
+        self.output_path.mkdir(exist_ok=True)
+        
+        # instantiate transforms, datasets
+        self.text_transform = self._build_text_transform()
+        self.image_transform = self._build_image_transform()
+        self.train_dataset = self._build_dataset("train_path")
+        self.dev_dataset = self._build_dataset("dev_path")
+        
+        # set up model and training
+        self.model = self._build_model()
+        self.trainer_params = self._get_trainer_params()
+    
+    ## Required LightningModule Methods (when validating) ##
+    
+    def forward(self, text, image, label=None):
+        return self.model(text, image, label)
+
+    def training_step(self, batch, batch_nb):
+        preds, loss = self.forward(
+            text=batch["text"], 
+            image=batch["image"], 
+            label=batch["label"]
+        )
+        self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        return {"loss": loss}
+
+    def training_epoch_end(self, outputs):
+        pass
+        
+    def test_step(self, batch, batch_nb):
+        preds, loss = self.forward(
+            text=batch["text"], 
+            image=batch["image"], 
+            label=batch["label"]
+        )
+        self.log("test_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return {"test_loss": loss}
+    
+    def test_epoch_end(self, outputs):
+        self.trainer.progress_bar_callback.main_progress_bar.write(
+            f"Epoch {self.trainer.current_epoch} test loss={self.trainer.progress_bar_dict['loss']}")
+        
+    def validation_step(self, batch, batch_nb):
+        preds, loss = self.eval().forward(
+            text=batch["text"], 
+            image=batch["image"], 
+            label=batch["label"]
+        )
+        self.log("val_loss",loss,on_step=True,on_epoch=True, prog_bar=True, logger=True)
+        
+        return {"batch_val_loss": loss}
+        
+    def validation_epoch_end(self, outputs):
+        pass
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(
+            self.model.parameters(), 
+            lr=self.hparams.get("lr", 0.001)
+        )
+        
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer
+        )
+        
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": scheduler,
+                "monitor": "val_loss",
+                "strict": False
+                # If "monitor" references validation metrics, then "frequency" should be set to a
+                # multiple of "trainer.check_val_every_n_epoch".
+          },
+        }
+
+    
+    def train_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.train_dataset, 
+            shuffle=True, 
+            batch_size=self.hparams.get("batch_size", 4), 
+            num_workers=self.hparams.get("num_workers", 16)
+        )
+
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(
+            self.dev_dataset, 
+            shuffle=False, 
+            batch_size=self.hparams.get("batch_size", 4), 
+            num_workers=self.hparams.get("num_workers", 16)
+        )
+    
+    ## Convenience Methods ##
+    
+    def fit(self):
+        self._set_seed(self.hparams.get("random_state", 42))
+        self.trainer = pl.Trainer(**self.trainer_params)
+        self.trainer.fit(self)
+        
+    def _set_seed(self, seed):
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(seed)
+
+    def _build_text_transform(self):
+        with tempfile.NamedTemporaryFile() as ft_training_data:
+            ft_path = Path(ft_training_data.name)
+            with ft_path.open("w") as ft:
+                training_data = [
+                    json.loads(line)["text"] + "/n" 
+                    for line in open(
+                        self.hparams.get("train_path")
+                    ).read().splitlines()
+                ]
+                for line in training_data:
+                    ft.write(line + "\n")
+                language_transform = fasttext.train_unsupervised(
+                    str(ft_path),
+                    model=self.hparams.get("fasttext_model", "cbow"),
+                    dim=self.embedding_dim
+                )
+        return language_transform
+    
+    def _build_image_transform(self):
+        image_dim = self.hparams.get("image_dim", 224)
+        image_transform = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.Resize(
+                    size=(image_dim, image_dim)
+                ),        
+                torchvision.transforms.ToTensor(),
+                # all torchvision models expect the same
+                # normalization mean and std
+                # https://pytorch.org/docs/stable/torchvision/models.html
+                torchvision.transforms.Normalize(
+                    mean=(0.485, 0.456, 0.406), 
+                    std=(0.229, 0.224, 0.225)
+                ),
+            ]
+        )
+        return image_transform
+
+    def _build_dataset(self, dataset_key):
+        return HatefulMemesDataset(
+            data_path=self.hparams.get(dataset_key, dataset_key),
+            img_dir=self.hparams.get("img_dir"),
+            image_transform=self.image_transform,
+            text_transform=self.text_transform,
+            # limit training samples only
+            dev_limit=(
+                self.hparams.get("dev_limit", None) 
+                if "train" in str(dataset_key) else None
+            ),
+            balance=True if "train" in str(dataset_key) else False,
+        )
+    
+    def _build_model(self):
+        # we're going to pass the outputs of our text
+        # transform through an additional trainable layer
+        # rather than fine-tuning the transform
+        language_module = torch.nn.Linear(
+                in_features=self.embedding_dim,
+                out_features=self.language_feature_dim
+        )
+        
+        # easiest way to get features rather than
+        # classification is to overwrite last layer
+        # with an identity transformation, we'll reduce
+        # dimension using a Linear layer, resnet is 2048 out
+        vision_module = torchvision.models.resnet152(
+            pretrained=True
+        )
+        for param in vision_module.parameters():
+            param.requires_grad = False
+        
+        vision_module.fc = torch.nn.Linear(
+                in_features=2048,
+                out_features=self.vision_feature_dim
+        )
+
+        return LanguageAndVisionConcat(
+            num_classes=self.hparams.get("num_classes", 2),
+            loss_fn=torch.nn.CrossEntropyLoss(),
+            language_module=language_module,
+            vision_module=vision_module,
+            language_feature_dim=self.language_feature_dim,
+            vision_feature_dim=self.vision_feature_dim,
+            fusion_output_size=self.hparams.get(
+                "fusion_output_size", 512
+            ),
+            dropout_p=self.hparams.get("dropout_p", 0.1),
+        )
+    
+    def _get_trainer_params(self):
+        checkpoint_callback = pl.callbacks.ModelCheckpoint(
+            # filepath=self.output_path,
+            dirpath=self.output_path,
+            monitor=self.hparams.get(
+                "checkpoint_monitor", "val_loss"
+            ),
+            mode=self.hparams.get(
+                "checkpoint_monitor_mode", "min"
+            ),
+            verbose=self.hparams.get("verbose", True)
+        )
+
+        early_stop_callback = pl.callbacks.EarlyStopping(
+            monitor=self.hparams.get(
+                "early_stop_monitor", "val_loss"
+            ),
+            min_delta=self.hparams.get(
+                "early_stop_min_delta", 0.001
+            ),
+            patience=self.hparams.get(
+                "early_stop_patience", 6
+            ),
+            verbose=self.hparams.get("verbose", True),
+        )
+
+        trainer_params = {
+            # "checkpoint_callback": checkpoint_callback,
+            "callbacks": [early_stop_callback,checkpoint_callback],
+            "default_root_dir": self.output_path,
+            "accumulate_grad_batches": self.hparams.get(
+                "accumulate_grad_batches", 1
+            ),
+            "accelerator":"gpu",
+            "devices":1,
+            "gpus": self.hparams.get("n_gpu", 1),
+            "max_epochs": self.hparams.get("max_epochs", 100),
+            "gradient_clip_val": self.hparams.get(
+                "gradient_clip_value", 1
+            ),
+        }
+        return trainer_params
+            
+    @torch.no_grad()
+    def make_submission_frame(self, test_path):
+        test_dataset = self._build_dataset(test_path)
+        submission_frame = pd.DataFrame(
+            index=test_dataset.samples_frame.id,
+            columns=["proba", "label"]
+        )
+        test_dataloader = torch.utils.data.DataLoader(
+            test_dataset, 
+            shuffle=False, 
+            batch_size=self.hparams.get("batch_size", 4), 
+            num_workers=self.hparams.get("num_workers", 16))
+        for batch in tqdm(test_dataloader, total=len(test_dataloader)):
+            preds, _ = self.model.eval().to("cpu")(
+                batch["text"], batch["image"]
+            )
+            submission_frame.loc[batch["id"], "proba"] = preds[:, 1]
+            submission_frame.loc[batch["id"], "label"] = preds.argmax(dim=1)
+        submission_frame.proba = submission_frame.proba.astype(float)
+        submission_frame.label = submission_frame.label.astype(int)
+        return submission_frame
+
+data_dir = "/home/nhd7682/hateful_memes"
+print(data_dir)
+data_dir = Path.cwd()/ "hateful_memes"
+img_tar_path = data_dir / "img.tar.gz"
+train_path = data_dir / "train.jsonl"
+dev_path = data_dir / "dev_seen.jsonl"
+test_path = data_dir / "test_seen.jsonl"
+
+train_samples_frame = pd.read_json(train_path, lines=True)
+train_samples_frame.label.value_counts()
+
+hparams = {
+    
+    # Required hparams
+    "train_path": train_path,
+    "dev_path": dev_path,
+    "img_dir": data_dir,
+    
+    # Optional hparams
+    "embedding_dim": 150,
+    "language_feature_dim": 300,
+    "vision_feature_dim": 300,
+    "fusion_output_size": 256,
+    "output_path": "model-outputs",
+    "dev_limit": None,
+    "lr": 0.00005,
+    "max_epochs": 30,
+    "n_gpu": 1,
+    "batch_size": 4,
+    # allows us to "simulate" having larger batches 
+    "accumulate_grad_batches": 16,
+    "early_stop_patience": 6,
+}
+if args.load:
+    load_path = Path.cwd()/hparams.get("output_path")/"epoch=1-step=190-v2.ckpt"
+    isExist = Path(load_path).is_file()
+    if not isExist:
+        raise Exception(f"No file exists at the path{load_path}, maybe you are missing saved model. Try training by running the script without load flag")
+    hateful_memes_model = HatefulMemesModel.load_from_checkpoint(load_path)
+    print(f"Model loaded from {load_path}")
+    hateful_memes_model.eval()
+    # TODO: Implement image perturbation attacks here
+    #       Implement text perturbation attacks here
+    #       Implement adversarial retraining here
+else:
+    hateful_memes_model = HatefulMemesModel(hparams=hparams)
+    hateful_memes_model.fit()
+    checkpoints = list(Path("model-outputs/lightning_logs/version_4/checkpoints").glob("*.ckpt"))
+    submission = hateful_memes_model.make_submission_frame(test_path)
+    submission.head()
+    submission.to_csv(("model-outputs/submission.csv"), index=True)
+    columns = ["id", "label"]
+    df = pd.read_csv("./model-outputs/submission.csv", usecols=columns)
+    import json
+    data = pd.read_json(test_path, lines=True)
+    accuracy = 0
+    row,col = df.shape
+    for csv_row in range(row):
+        if(df.label[csv_row] == data.label[csv_row]):
+            accuracy+=1
+    print("%.2f%%"%((accuracy/1000) * 100) )
